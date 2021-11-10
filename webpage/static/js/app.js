feacbb9aa2f72f3dee48a5349aeb65b504acd06b
Frank <franfd_98@hotmail.com>
2021-11-09 23:48:22 -0300

    todo

---
+++ webpage/static/js/app.js
@@ -0,0 +1,12 @@
+// Function for the toggle menu
+
+var navLinks = document.getElementById("navLinks")
+
+function showMenu(){
+    navLinks.style.right = "0";
+}
+
+function closeMenu(){
+    navLinks.style.right = "-200px";
+}
+
